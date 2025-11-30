17f6ad7767459c3e0da6fbe245dacc74d431b4b1
François Chaix <fr.chaix@gmail.com>
2025-11-30 22:42:40 +0100

    blend color for the diffs

--- lua/compline/theme.lua
+++ lua/compline/theme.lua
@@ -1,3 +1,29 @@
+local function blend(fg, bg, alpha)
+  local function hex_to_rgb(hex)
+    hex = hex:gsub("#", "")
+    return {
+      tonumber(hex:sub(1, 2), 16),
+      tonumber(hex:sub(3, 4), 16),
+      tonumber(hex:sub(5, 6), 16)
+    }
+  end
+
+  local function rgb_to_hex(rgb)
+    return string.format("#%02x%02x%02x", rgb[1], rgb[2], rgb[3])
+  end
+
+  local f = hex_to_rgb(fg)
+  local b = hex_to_rgb(bg)
+
+  local blend_rgb = {
+    math.floor((alpha * f[1]) + ((1 - alpha) * b[1])),
+    math.floor((alpha * f[2]) + ((1 - alpha) * b[2])),
+    math.floor((alpha * f[3]) + ((1 - alpha) * b[3])),
+  }
+
+  return rgb_to_hex(blend_rgb)
+end
+
 local M = {}
 
 function M.setup(p)
@@ -37,9 +63,15 @@ function M.setup(p)
   hi("DiagnosticHint",  { fg = p.teal })
 
   -- === Diff ===
-  hi("DiffAdd",    { bg = p.green .. "20" })
-  hi("DiffDelete", { bg = p.red .. "20" })
-  hi("DiffChange", { bg = p.blue .. "20" })
+hi("DiffAdd", {
+  bg = blend(p.green, p.bg, 0.15)
+})
+hi("DiffDelete", {
+  bg = blend(p.red, p.bg, 0.15)
+})
+hi("DiffChange", {
+  bg = blend(p.blue, p.bg, 0.15)
+})
 end
 
 return M
